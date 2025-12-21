17a0a51df46163bef48a37202d093f78bd45a467
Hani <121813295+aoyyyv@users.noreply.github.com>
2025-12-21 19:06:47 +0300

    Files

---
+++ index.js
@@ -0,0 +1,39 @@
+let counter = document.getElementById("counter")
+let counter2 = document.getElementById("counter2")
+let count = 0
+let count2 = 0
+
+
+function add1() {
+   count += 1
+    counter.textContent = count
+}
+
+function add2() {
+    count += 2
+    counter.textContent = count
+}
+
+function add3() {
+    count += 3
+    counter.textContent = count
+}
+
+//===========================================
+
+function addone() {
+   count2 += 1
+    counter2.textContent = count2
+}
+
+function addtwo() {
+    count2 += 2
+    counter2.textContent = count2
+}
+
+function addthree() {
+    count2 += 3
+    counter2.textContent = count2
+}
+
+
